aade29eb9e137137b2f23c6feb9e9ba141818a2d
julianVega0323 <37226558+julianVega0323@users.noreply.github.com>
2018-12-16 19:47:17 -0500

    Añadiendo JavaScript

---
+++ js.js
@@ -0,0 +1,9 @@
+$('#link-todas').click(function(event) {
+	$('#contenedor').load('BasesBibliograficas.html');
+});
+$('#link-gestores').click(function(event) {
+	$('#contenedor').load('GestoresBibliograficos.html');
+});
+$('#link-mendeley').click(function(event) {
+	$('#contenedor').load('TutorialMendeley.html');
+});
